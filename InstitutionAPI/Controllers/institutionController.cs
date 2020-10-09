b470a0dbc18606e4d94872d218c54e85e9aaad69
firas-git-hub <72254583+firas-git-hub@users.noreply.github.com>
2020-10-09 16:14:00 +0300

    Add files via upload

---
+++ InstitutionAPI/Controllers/institutionController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using InstitutionAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace InstitutionAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class institutionController : ControllerBase
+    {
+        private readonly institutionContext _context;
+
+        public institutionController(institutionContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("{institutionId}")]
+        public async Task<ActionResult<institution>> GetInstitution(Guid institutionId)
+        {
+            var institution = await _context.institutions.FindAsync(institutionId);
+
+            if (institution == null)
+            {
+                return NotFound();
+            }
+
+            return institution;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<institution>>> GetInstitutitons()
+        {
+            return await _context.institutions
+               .ToListAsync();
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateTodoItem(Guid id, institution institution)
+        {
+            if (id != institution.Id)
+            {
+                return BadRequest();
+            }
+
+            var todoItem = await _context.institutions.FindAsync(id);
+            if (todoItem == null)
+            {
+                return NotFound();
+            }
+
+            todoItem.Name = institution.Name;
+            todoItem.Code = institution.Code;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException) when (!todoItemExists(id))
+            {
+                return NotFound();
+            }
+
+            return NoContent();
+        }
+
+
+        [HttpPost]
+        public ActionResult<institution> CreateInstitution(institution inst)
+        {
+            var institution = new institution
+            {
+                Code = inst.Code,
+                Name = inst.Name
+            };
+            _context.institutions.Add(institution);
+            _context.SaveChangesAsync();
+
+            return Created(
+                nameof(GetInstitution),
+                institution);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteInstitution(Guid id)
+        {
+            var institution = await _context.institutions.FindAsync(id);
+
+            if (institution == null)
+            {
+                return NotFound();
+            }
+
+            _context.institutions.Remove(institution);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+        private bool todoItemExists(Guid id)
+        {
+            return _context.institutions.Any(e => e.Id == id);
+        }
+    }  
+}
---
+++ InstitutionAPI/Models/institutionContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.SqlServer;
+
+namespace InstitutionAPI.Models
+{
+    public class institutionContext : DbContext
+    {
+        public DbSet<institution> institutions { get; set; }
+        public institutionContext(DbContextOptions<institutionContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseNpgsql("Server=localhost;Database=institutionDB;password=BatistaBomb_96;username=postgres");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+    }
+}
